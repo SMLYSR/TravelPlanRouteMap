bff2375f5124fceab4b5379721c39682f2f81168
zhangchengbin183 <zhangchengbin183@hellobike.com>
2026-01-17 21:24:06 +0800

    Initial Commit

---
+++ TravelPlanRouteMap/TravelPlanRouteMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelPlanRouteMapApp.swift
+//  TravelPlanRouteMap
+//
+//  Created by zhangcb on 2026/1/17.
+//
+
+import SwiftUI
+
+@main
+struct TravelPlanRouteMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
